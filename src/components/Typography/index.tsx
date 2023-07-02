a738fde5df6898d7b4623d6ed6c35b659d08c097
Thalya Stéffany <thalyacoiimbra@gmail.com>
2023-07-01 21:12:22 -0300

    feat(components): cria componente Typography

---
+++ src/components/Typography/index.tsx
@@ -0,0 +1,36 @@
+import {PropsWithChildren} from "react";
+import ITypographyProps from "../../interfaces/ITypography";
+
+export default function Typography({
+    htmlFor,
+    as: TagName = "p", 
+    color, 
+    weight = "extralight",
+    size = "sm", 
+    children
+}: PropsWithChildren<ITypographyProps>) {
+    const sizes = {
+        xs: "text-sm",
+        sm: "text-lg",
+        md: "text-3xl",
+        lg: "text-4xl"
+    };
+    const colors = {
+        secondary: "opacity-80",
+        danger: "text-red-500",
+    };
+
+    return (
+        <TagName 
+        {...htmlFor && {htmlFor: htmlFor}}
+        className={`
+            tracking-wide     
+            ${color !== undefined ? colors[color] : ""}
+            ${weight === "bold" ? "font-bold" : "font-extralight"}
+            ${sizes[size]}
+        `}
+        >
+            {children}
+        </TagName>
+    )
+}
---
+++ src/interfaces/ITypography.ts
@@ -0,0 +1,11 @@
+import { THeadingTags } from "./IHeadingGroup";
+
+export type TTypographySizes = "xs" | "sm" | "md" | "lg";
+
+export default interface ITypographyProps {    
+    as: THeadingTags | "p" | "span" | "label";
+    color?: "secondary" | "danger";
+    weight?: "extralight" | "bold";
+    size?: TTypographySizes;
+    htmlFor?: string;
+}
