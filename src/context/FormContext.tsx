1ca649f7779f81dfbae7cb86f3c323322ecf35b5
Thalya Stéffany <thalyacoiimbra@gmail.com>
2023-08-09 22:08:49 -0300

    feat(context): cria FormContext

--- src/context/FormContext.tsx
+++ src/context/FormContext.tsx
@@ -1,23 +1,22 @@
-import {  ReactNode, createContext, useContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import { IFormFields } from "../interfaces/IFormFields";
-import { IFormContext } from "../interfaces/IFormContext";
+import { IFormContext, IFormContextProviderProp } from "../interfaces/IFormContext";
+import { initialValues } from "./initialValues";
+import { useValidation } from "../hook/useValidation";
 
 const FormContext = createContext({} as IFormContext);
 
-export function FormProvider({children}:{children: ReactNode}) {
-    const [formValues, setFormValues] = useState<IFormFields>({
-        fullname: "",
-        email: "",
-        xp: "beginner",
-        github: ""
-    });
-  
+export function FormProvider({ children }: IFormContextProviderProp) {
+    const [formValues, setFormValues] = useState<IFormFields>(initialValues.values);
+    const [activeStep, setActiveStep] = useState(initialValues.activeStep);
+    const { errors, validateField } = useValidation();    
+   
     return (
-        <FormContext.Provider value={{formValues, setFormValues}}>
+        <FormContext.Provider value={{ formValues, setFormValues, activeStep, setActiveStep, validateField, errors }}>
             {children}
         </FormContext.Provider>
     )
-}
+};
 
 export function useFormContext() {
     return useContext(FormContext);
