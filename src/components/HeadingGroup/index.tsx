5e72bc5ccb073d49a6b9eb87b90a3160b8781b57
Thalya Stéffany <thalyacoiimbra@gmail.com>
2023-08-15 18:05:15 -0300

    fix(HeadingGroup): ajusta gapSize

--- src/components/HeadingGroup/index.tsx
+++ src/components/HeadingGroup/index.tsx
@@ -3,7 +3,7 @@ import IHeadingGroupProps from "../../interfaces/IHeadingGroup";
 
 export default function HeadingGroup({title, subTitle, gapSize = "sm"}: IHeadingGroupProps) {
     return (
-        <div className={`flex flex-col ${gapSize === "sm" ? "gap-1" : "gap-5"}`}>
+        <div className={`flex flex-col ${gapSize === "sm" ? "gap-2" : "gap-5"}`}>
             <Typography as={title.as} weight="bold" size={title.size}>{title.text}</Typography>
             <Typography as={subTitle.as} color="secondary">{subTitle.text}</Typography>
         </div>
