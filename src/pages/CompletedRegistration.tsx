12ef273bd42abde7683c914c9bcac550f9936cc1
Thalya Stéffany <thalyacoiimbra@gmail.com>
2023-08-27 07:52:08 -0300

    feat(CompletedRegistration): cria estilização da página

--- src/pages/CompletedRegistration.tsx
+++ src/pages/CompletedRegistration.tsx
@@ -3,10 +3,10 @@ import Typography from "../components/Typography";
 
 export default function CompletedRegistration() {
     return (
-        <div>             
+        <div className="flex flex-col justify-center items-center h-screen gap-4">             
             <Typography as="h1" size="xl">Seu cadastro foi realizado com sucesso!</Typography>
             <Typography as="h3" size="md">Em breve você começará a receber vagas incríveis no seu e-mail.</Typography>
-            <Button hasBackground onClickFunc={() => console.log('fechar')}>Fechar aba</Button>
+            <Button className="mt-8" hasBackground onClickFunc={() => console.log('fechar')}>Fechar aba</Button>
         </div>
     );
 };
