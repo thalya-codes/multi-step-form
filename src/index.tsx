b90bba2524b9356b64443464d598726721617ae5
Thalya Stéffany <thalyacoiimbra@gmail.com>
2023-09-06 19:08:14 -0300

    feat(router): cria rotas da aplicação

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import "./global/index.scss";
 import { FormProvider } from './context/FormContext';
+import { Router } from './router/Router';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <FormProvider>
-      <App />
+      <Router />
     </FormProvider>
   </React.StrictMode>
 );
