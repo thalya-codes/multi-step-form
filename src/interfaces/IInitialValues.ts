161b545a389c99fee8a398043fcd9c2fa16e5c57
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2023-11-29 06:32:21 -0300

    refactor(IInitialValues): exclui prop errors

--- src/interfaces/IInitialValues.ts
+++ src/interfaces/IInitialValues.ts
@@ -1,8 +1,6 @@
 import { IFormFields } from "./IFormFields";
-import { IFormErrors } from "./IUseValidation";
 
 export interface IInitialValues {
     values: IFormFields;
     activeStep: number;
-    errors: IFormErrors;
 };
