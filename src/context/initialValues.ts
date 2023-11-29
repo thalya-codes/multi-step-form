3b30800bdd1fcb22211c4a945fab27583358b9c7
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2023-11-29 06:30:05 -0300

    refactor(cotext/initialValues): exclui valores iniciais do obj de errors

--- src/context/initialValues.ts
+++ src/context/initialValues.ts
@@ -1,16 +1,11 @@
 import { IInitialValues } from "../interfaces/IInitialValues";
 
 export const initialValues: IInitialValues = {
-    values: {
-      fullname: "",
-      github: "",
-      email: "",
-      xp: "beginner"
-    },  
-    errors: {
-      fullname: "",
-      github: "",
-      email: ""
-    },
-    activeStep: 1
+  values: {
+    fullname: "",
+    github: "",
+    email: "",
+    xp: "beginner"
+  }, 
+  activeStep: 1
 };
