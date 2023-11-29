d7fab0f80fa2f25fc84d7589cb2ad5f3b0c1dd4c
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2023-11-29 06:28:02 -0300

    refactor(useValidation): altera estrutura de dados Altera estrutura de dados utilizada no state errors

--- src/hook/useValidation.ts
+++ src/hook/useValidation.ts
@@ -1,23 +1,12 @@
 import { useState } from "react";
-import { IFormErrors, IValidateFieldParams, IValidatePrevStepsParams, TFieldNamesToValidate } from "../interfaces/IUseValidation";
-import { initialValues } from "../context/initialValues";
-import { useTranslation } from "react-i18next";
+import { IValidateFieldParams, IValidatePrevStepsParams, TFieldNamesToValidate } from "../interfaces/IUseValidation";
 
 export function useValidation() {
-  const [errors, setErrors] = useState<IFormErrors>(initialValues.errors);
-  const { t: translation } = useTranslation();
-
-  const ErrorsMgs ={  
-    fullname: translation("steps.Please, enter your full name"),
-    email:  translation("steps.Email format is invalid!"),
-    github:  translation("steps.The given address is incomplete or not from GitHub!"),
-    emptyField:  translation("general.The field cannot be empty!")
-  };
+  const [errors, setErrors] = useState<Set<TFieldNamesToValidate>>(new Set());
 
   const validateField = ({ name, value }: IValidateFieldParams): boolean => {      
     const isEmpty: boolean = value === "";
     let isValid: boolean = !isEmpty;
-    const errorMessage: string = isEmpty ? ErrorsMgs['emptyField'] : ErrorsMgs[name];
       
     switch(name) {
       case "email": 
@@ -33,10 +22,18 @@ export function useValidation() {
         break;
     };
 
-    setErrors((prevErrors) => ({ ...prevErrors, [name]: !isValid ? errorMessage : "" }));
+    setErrors((prevErrors: Set<TFieldNamesToValidate>) => {
+      const newErrorSet: Set<TFieldNamesToValidate> = new Set(prevErrors);
 
+      if (!isValid) newErrorSet.add(name);
+      else newErrorSet.delete(name);
+      
+      return newErrorSet;
+    });
+    
     return isValid;     
   }; 
+
 
   const validatePrevSteps = ({ stepNumber, fieldNamesByStep, formValues }: IValidatePrevStepsParams): boolean | null => {
     let isValid = validateField({ name: 'fullname', value: formValues['fullname'] });
