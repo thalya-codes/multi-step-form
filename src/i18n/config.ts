e6068f07b2506173b0434dfb1baf79d4435df64e
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-02-27 21:56:50 -0300

    feat(i18n/config): adiciona supported and default languages

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,21 +1,22 @@
-import i18next from "i18next";
+import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from "i18next-browser-languagedetector";
-import ptBR from "./locales/pt-BR.json";
-import enUS from "./locales/en-US.json";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import ptBR from './locales/pt-BR.json';
+import enUS from './locales/en-US.json';
 
-export 
-default i18next
-    .use(initReactI18next)
-    .use(LanguageDetector)
-    .init({
-        resources: {
-            pt: {
-                translation: ptBR
-            },
-            en: {
-                translation: enUS
-            }
-        },
-        interpolation: { escapeValue: false }
-    });
+export default i18next
+ .use(initReactI18next)
+ .use(LanguageDetector)
+ .init({
+  resources: {
+   'pt-BR': {
+    translation: ptBR,
+   },
+   'en': {
+    translation: enUS,
+   },
+  },
+  supportedLngs: ['en', 'pt-BR'],
+  fallbackLng: 'en',
+  interpolation: { escapeValue: false },
+ });
